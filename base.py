1cac7b53a3255f9066778ed6d0fe88185c83552c
Maxence GAUTIER <maxence.gautier@tpsgroussard.loc>
2024-08-21 15:36:20 +0200

    Initial commit

---
+++ base.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name} for user in users])
+
+if __name__ == '__main__':
+    app.run(debug=True)
